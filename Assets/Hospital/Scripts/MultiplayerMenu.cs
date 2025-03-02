fc97a02bbbad1d97bb01f012c188b91a031c8b0d
YatagarasuGames <yatagarasugames5@gmail.com>
2025-03-02 17:25:01 +0300

    Добавлено кастомное мультиплеерное меню(надеюсь работает)

---
+++ Assets/Hospital/Scripts/MultiplayerMenu.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+using UnityEngine.UI;
+
+public class MultiplayerMenu : MonoBehaviour
+{
+    [SerializeField] private NetworkManager _networkManager;
+    [SerializeField] private InputField _IPinputField;
+    [SerializeField] private InputField _portInputField;
+
+    public void StartHost()
+    {
+        _networkManager.StartHost();
+    }
+
+    public void StartClient()
+    {
+        _networkManager.StartClient();
+        _networkManager.networkAddress = _IPinputField.text;
+        if (Transport.active is PortTransport portTransport)
+        {
+            if (ushort.TryParse(_portInputField.ToString(), out ushort port))
+                portTransport.Port = port;
+        }
+    }
+}
