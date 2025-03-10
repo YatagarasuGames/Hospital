06397e66675fb542e390e51b70e49eab53535621
YatagarasuGames <yatagarasugames5@gmail.com>
2025-03-11 02:33:12 +0300

    Добавлено меню лобби + возможность к ним подключаться(все лоббиSpacewar но бтв)

--- Assets/Hospital/Scripts/SteamLobby.cs
+++ Assets/Hospital/Scripts/SteamLobby.cs
@@ -7,13 +7,26 @@ using System.Security.Cryptography;
 
 public class SteamLobby : MonoBehaviour
 {
+    public static SteamLobby Instance;
+
     protected Callback<LobbyCreated_t> LobbyCreated;
     protected Callback<GameLobbyJoinRequested_t> JoinRequest;
     protected Callback<LobbyEnter_t> LobbyEntered;
 
+
+    protected Callback<LobbyMatchList_t> LobbyList;
+    protected Callback<LobbyDataUpdate_t> LobbyDataUpdated;
+
+    public List<CSteamID> lobbyIDs = new List<CSteamID>();
+
     public ulong CurrentLobbyId;
     private const string HostAddressKey = "HostAddress";
     private NetworkManager networkManager;
+
+    private void Awake()
+    {
+        if(Instance == null) Instance = this;
+    }
 
     private void Start()
     {
@@ -22,11 +35,15 @@ public class SteamLobby : MonoBehaviour
         LobbyCreated = Callback<LobbyCreated_t>.Create(OnLobbyCreated);
         JoinRequest = Callback<GameLobbyJoinRequested_t>.Create(OnJoinRequest);
         LobbyEntered = Callback<LobbyEnter_t>.Create(OnLobbyEntered);
+
+
+        LobbyList = Callback<LobbyMatchList_t>.Create(OnGetLobbyList);
+        LobbyDataUpdated = Callback<LobbyDataUpdate_t>.Create(OnGetLobbyData);
     }
 
     public void HostLobby()
     {
-        SteamMatchmaking.CreateLobby(ELobbyType.k_ELobbyTypeFriendsOnly, networkManager.maxConnections);
+        SteamMatchmaking.CreateLobby(ELobbyType.k_ELobbyTypePublic, networkManager.maxConnections);
     }
 
     private void OnLobbyCreated(LobbyCreated_t callback)
@@ -53,5 +70,36 @@ public class SteamLobby : MonoBehaviour
         networkManager.networkAddress = SteamMatchmaking.GetLobbyData(new CSteamID(callback.m_ulSteamIDLobby), HostAddressKey);
 
         networkManager.StartClient();
+    }
+
+    public void JoinLobby(CSteamID lobbyID)
+    {
+        SteamMatchmaking.JoinLobby(lobbyID);
+    }
+
+
+    public void GetLobbiesList()
+    {
+        if(lobbyIDs.Count > 0) lobbyIDs.Clear();
+
+        SteamMatchmaking.AddRequestLobbyListResultCountFilter(60);
+        SteamMatchmaking.RequestLobbyList();
+    }
+
+    private void OnGetLobbyList(LobbyMatchList_t result)
+    {
+        if (LobbiesListManager.instance.listOfLobbies.Count > 0) LobbiesListManager.instance.DestroyLobbies();
+
+        for(int i = 0; i<result.m_nLobbiesMatching; i++)
+        {
+            CSteamID lobbyID = SteamMatchmaking.GetLobbyByIndex(i);
+            lobbyIDs.Add(lobbyID);
+            SteamMatchmaking.RequestLobbyData(lobbyID);
+        }
+    }
+
+    private void OnGetLobbyData(LobbyDataUpdate_t result)
+    {
+        LobbiesListManager.instance.DisplayLobbies(lobbyIDs, result);
     }
 }
